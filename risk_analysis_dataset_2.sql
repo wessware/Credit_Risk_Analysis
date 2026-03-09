b6a518be7639c372c6fcf50f188df5d86e1d7587
wessware <stevenshisia@yahoo.com>
2026-03-09 21:26:50 +0300

    First test model - rule-based

--- risk_analysis_dataset_2.sql
+++ risk_analysis_dataset_2.sql
@@ -72,6 +72,88 @@ FROM risk_analysis_dataset
 GROUP BY Payment_Behaviour
 ORDER BY Frequency DESC;
 
+
+-- FIRST TEST MODEL
+
+SELECT *, 
+
+CASE
+WHEN Composite_Credit_Risk_Score >= 0.75 THEN "LOW RISK"
+WHEN Composite_Credit_Risk_Score >= 0.50 THEN "MODERARE RISK"
+WHEN Composite_Credit_Risk_Score >= 0.25 THEN "HIGH RISK"
+ELSE "VERY HIGH RISK"
+
+END AS Credit_Risk_Category
+
+FROM 
+(
+    SELECT *,
+    (
+    /* DEBT TO INCOME RATIO */
+    (1 - 
+    CASE
+        WHEN Outstanding_Debt / (Annual_Income+1) > 1 THEN 1
+        WHEN Outstanding_Debt / (Annual_Income+1) < 0 THEN 0
+        ELSE Outstanding_Debt / (Annual_Income+1)
+    END
+    ) * 0.25
+
++
+
+    /* EMI CREDIT PRESSURE EVALUATION */
+    (1 - 
+    CASE
+        WHEN Total_EMI_per_month / (Monthly_Inhand_Salary+1) > 1 THEN 1
+        WHEN Total_EMI_per_month / (Monthly_Inhand_Salary+1) < 0 THEN 0
+        ELSE Total_EMI_per_month / (Monthly_Inhand_Salary+1)
+    END
+    ) * 0.20
+
++ 
+
+    /* DELINQUENCY */
+    (1 - 
+    CASE
+        WHEN Num_of_Delayed_Payment / (Num_of_Loan+1) > 1 THEN 1
+        WHEN Num_of_Delayed_Payment / (Num_of_Loan+1) < 0 THEN 0
+        ELSE Num_of_Delayed_Payment / (Num_of_Loan+1)
+    END
+    ) * 0.20
+
++ 
+
+    /* CREDIT HISTORY NORMALIZATION */
+    CASE
+        WHEN Credit_History_Age / 120 > 1 THEN 1
+        WHEN Credit_History_Age / 120 < 0 THEN 0
+        ELSE Credit_History_Age / 120
+    END * 0.15
+
++ 
+    /* MONTHLY SAVING RATIO NORMALIZATION */
+    CASE
+        WHEN Monthly_Balance / (Monthly_Inhand_Salary+1) > 1 THEN 1
+        WHEN Monthly_Balance / (Monthly_Inhand_Salary+1) < 0 THEN 0
+        ELSE Monthly_Balance / (Monthly_Inhand_Salary+1)
+    END * 0.10
+
++ 
+
+    /* CREDIT UTILIZATION NORMALIZATION */
+    (1 - 
+    CASE
+        WHEN Credit_Utilization_Ratio > 1 THEN 1
+        WHEN Credit_Utilization_Ratio < 0 THEN 0
+        ELSE Credit_Utilization_Ratio
+    END
+    ) * 0.10
+
+) AS Composite_Credit_Risk_Score
+
+FROM risk_analysis_dataset
+
+) t;
+
 -- FEATURE NORMALIZATION
 
 SELECT *,

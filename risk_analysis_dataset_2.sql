311673debefce7a781a32a8f3c50ad69d57f286e
wessware <stevenshisia@yahoo.com>
2026-03-09 21:29:28 +0300

    First rule-based model with normalized features and credit category upated

--- risk_analysis_dataset_2.sql
+++ risk_analysis_dataset_2.sql
@@ -206,3 +206,52 @@ SELECT *,
 
             FROM risk_analysis_dataset
         ) t;
+
+-- FIRST MODEL WITH NORMALIZED FEATURES AND CREDIT CATEGORIES
+
+SELECT *,
+    CASE
+        WHEN Composite_Credit_Risk_Score >= 0.75 THEN "LOW RISK"
+        WHEN Composite_Credit_Risk_Score >= 0.50 THEN "MODERATE RISK"
+        WHEN Composite_Credit_Risk_Score >= 0.25 THEN "HIGH RISK"
+        ELSE "VERY HIGH RISK"
+
+    END AS Credit_Risk_Category
+
+FROM
+(
+    SELECT *,
+
+        (1 - normalized_dti)            * 0.25 
+        + 
+        (1 - normalized_emi)            * 0.20
+        +
+        (1 - normalized_delinquency)    * 0.20
+        +
+        normalized_credit_history       * 0.15 
+        +
+        normalized_savings              * 0.10
+        +
+        (1 - normalized_utilization)    * 0.10
+        
+        AS Composite_Credit_Risk_Score
+
+    FROM 
+    (
+        SELECT *,
+
+            GREATEST(0, LEAST(Outstanding_Debt / (Annual_Income+1), 1)) AS normalized_dti,
+
+            GREATEST(0, LEAST(Total_EMI_per_month / (Monthly_Inhand_Salary+1), 1)) AS normalized_emi,
+
+            GREATEST(0, LEAST(Num_of_Delayed_Payment / (Num_of_Loan+1), 1)) AS normalized_delinquency,
+
+            GREATEST(0, LEAST(Credit_History_Age/120, 1)) AS normalized_credit_history,
+
+            GREATEST(0, LEAST(Monthly_Balance / (Monthly_Inhand_Salary+1), 1)) AS normalized_savings, 
+
+            GREATEST(0, LEAST(Credit_Utilization_Ratio, 1)) AS normalized_utilization
+
+        FROM risk_analysis_dataset
+    ) normalized
+) scored;

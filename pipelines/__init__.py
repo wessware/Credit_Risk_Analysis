47eb3ca25ca54263094d596dfca3d7b783e19c79
wessware <stevenshisia@yahoo.com>
2026-04-22 17:13:55 +0300

    Directory change upstream updates

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,4 +1,4 @@
-from .kingametric_xgb_pipeline import KingaMetricXGB
-from .kingametric_lgbm_pipeline import KingaMetricLGBM
-from .kingametric_rf_pipeline import KingaMetricRF
+from .Archived.kingametric_xgb_pipeline import KingaMetricXGB
+from .Archived.kingametric_lgbm_pipeline import KingaMetricLGBM
+from .Archived.kingametric_rf_pipeline import KingaMetricRF
 

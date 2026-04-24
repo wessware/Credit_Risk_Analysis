31fccdabf7237c3e3f40b560aae080d7b28bf839
wessware <stevenshisia@yahoo.com>
2026-04-24 08:27:37 +0300

    Adjusted pickled backend model to reflect latest updates in the pipeline

--- streamlit_apps/streamlit_xgb_0.py
+++ streamlit_apps/streamlit_xgb_0.py
@@ -22,7 +22,7 @@ st.divider()
 
 @st.cache_resource
 def load_model():
-    model_path = "pickled_models/kingametric_base_0.pkl"
+    model_path = "pickled_models/kingametric_base_1.pkl"
     model = joblib.load(model_path)
     raw_features = getattr(model, "raw_input_features", [])
     selected_features = getattr(model, "feature_names", [])
